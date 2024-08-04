4f3c021f03f5974cd945775c6e4fdbc5b96d3f0b
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-08-04 17:07:34 -0600

    update

--- components/triple_white_light/light.py
+++ components/triple_white_light/light.py
@@ -30,65 +30,53 @@ def to_code(config):
     cg.add(var.set_cool_white(cool_white))
 
     cg.add_define("USE_TRIPLE_WHITE_LIGHT")
+    cg.add_global(cg.RawExpression(f"""
+    class TripleWhiteLight : public Component, public LightOutput {{
+    public:
+  void set_amber(output::FloatOutput *amber) {{ amber_ = amber; }}
+  void set_warm_white(output::FloatOutput *warm_white) {{ warm_white_ = warm_white; }}
+  void set_cool_white(output::FloatOutput *cool_white) {{ cool_white_ = cool_white; }}
 
-    # Add the C++ code for the custom light
-    cg.add_global(triple_white_ns.using)
-    cg.add_library("Vector", None)
+  LightTraits get_traits() override {{
+    return {{false, false, false, true, 153, 500}};
+  }}
+
+  void write_state(LightState *state) override {{
+    float brightness = state->get_brightness();
+    float color_temp = state->get_color_temperature();
     
-    cg.add_global(cg.RawExpression("""
-    class TripleWhiteLight : public Component, public LightOutput {
-     public:
-      void set_amber(output::FloatOutput *amber) { amber_ = amber; }
-      void set_warm_white(output::FloatOutput *warm_white) { warm_white_ = warm_white; }
-      void set_cool_white(output::FloatOutput *cool_white) { cool_white_ = cool_white; }
-      
-      LightTraits get_traits() override {
-        auto traits = LightTraits();
-        traits.set_supports_brightness(true);
-        traits.set_supports_rgb(false);
-        traits.set_supports_rgb_white_value(false);
-        traits.set_supports_color_temperature(true);
-        traits.set_min_mireds(153);
-        traits.set_max_mireds(500);
-        return traits;
-      }
-      
-      void write_state(LightState *state) override {
-        float brightness, color_temp;
-        state->current_values_as_brightness(&brightness);
-        state->current_values_as_color_temperature(&color_temp);
-        
-        float amber, warm_white, cool_white;
-        if (color_temp <= 2000) {
-          amber = 1.0f;
-          warm_white = 0.0f;
-          cool_white = 0.0f;
-        } else if (color_temp <= 2700) {
-          amber = 1.0f - (color_temp - 2000) / 700;
-          warm_white = (color_temp - 2000) / 700;
-          cool_white = 0.0f;
-        } else if (color_temp <= 4600) {
-          amber = 0.0f;
-          warm_white = 1.0f - (color_temp - 2700) / 1900;
-          cool_white = (color_temp - 2700) / 1900;
-        } else if (color_temp <= 6500) {
-          amber = 0.0f;
-          warm_white = 0.5f - 0.5f * (color_temp - 4600) / 1900;
-          cool_white = 0.5f + 0.5f * (color_temp - 4600) / 1900;
-        } else {
-          amber = 0.0f;
-          warm_white = 0.0f;
-          cool_white = 1.0f;
-        }
-        
-        amber_->set_level(amber * brightness);
-        warm_white_->set_level(warm_white * brightness);
-        cool_white_->set_level(cool_white * brightness);
-      }
-      
-     protected:
-      output::FloatOutput *amber_;
-      output::FloatOutput *warm_white_;
-      output::FloatOutput *cool_white_;
-    };
-    """))
+    float amber = 0.0f, warm_white = 0.0f, cool_white = 0.0f;
+    if (color_temp <= 2000) {{
+      amber = 1.0f;
+      warm_white = 0.0f;
+      cool_white = 0.0f;
+    }} else if (color_temp <= 2700) {{
+      amber = 1.0f - (color_temp - 2000) / 700;
+      warm_white = (color_temp - 2000) / 700;
+      cool_white = 0.0f;
+    }} else if (color_temp <= 4600) {{
+      amber = 0.0f;
+      warm_white = 1.0f - (color_temp - 2700) / 1900;
+      cool_white = (color_temp - 2700) / 1900;
+    }} else if (color_temp <= 6500) {{
+      amber = 0.0f;
+      warm_white = 0.5f - 0.5f * (color_temp - 4600) / 1900;
+      cool_white = 0.5f + 0.5f * (color_temp - 4600) / 1900;
+    }} else {{
+      amber = 0.0f;
+      warm_white = 0.0f;
+      cool_white = 1.0f;
+    }}
+    
+    amber_->set_level(amber * brightness);
+    warm_white_->set_level(warm_white * brightness);
+    cool_white_->set_level(cool_white * brightness);
+  }}
+  
+ protected:
+  output::FloatOutput *amber_;
+  output::FloatOutput *warm_white_;
+  output::FloatOutput *cool_white_;
+}};
+"""))
+
